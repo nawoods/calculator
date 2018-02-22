cc6b6ae0690168a9f424fdfeeca368b17494c777
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-02-22 00:57:00 +0000

    Implement functionality for clear and individual number buttons

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function capitalize (str) {
     return str.slice(0, 1).toUpperCase + str.slice(1).toLowerCase;
 }
 
+// 'internal memory' of the calculator
+const calculatorState = {
+    number1: '0',
+    binaryOperator: '',
+    number2: '0',
+    clearOnEntry: false, // whether pressing any button clears the above variables
+}
+
 const buttonShortNames = ['plusminus', 'squareroot', 'percent', 'divide',
                           '7', '8', '9', 'multiply',
                           '4', '5', '6', 'subtract',
@@ -9,6 +17,7 @@ const buttonShortNames = ['plusminus', 'squareroot', 'percent', 'divide',
                           'clear', '0', 'decimal', 'equals'];
                           
 const buttons = {};
+const screen = document.querySelector('.calculator-screen')
 
 for (let i = 0; i < buttonShortNames.length; i++) {
     let shortName = buttonShortNames[i];
@@ -17,5 +26,38 @@ for (let i = 0; i < buttonShortNames.length; i++) {
 }
 
 function onButtonClick() {
-    console.log(this.innerHTML);
+    if (calculatorState.clearOnEntry) clear();
+    updateScreen(this.innerHTML);
+    
+    if (this.classList.contains('button-clear')) {
+        clear();
+        return;
+    }
+    
+    if (this.classList.contains('number')) {
+        onNumberButtonClick(this.innerHTML);
+        return;
+    }
+}
+
+function onNumberButtonClick(str) {
+    let currentNumber = 'number' + (calculatorState.binaryOperator === '' ? 1 : 2);
+    if (calculatorState[currentNumber] !== '0') {
+        calculatorState[currentNumber] += str;
+    } else if (str !== 0) {
+        calculatorState[currentNumber] = str;
+    }
+    updateScreen(calculatorState[currentNumber]);
+}
+
+function updateScreen(str) {
+    screen.innerHTML = str;
+}
+
+function clear() {
+    calculatorState.number1 = '0';
+    calculatorState.binaryOperator = '';
+    calculatorState.number2 = '0';
+    calculatorState.clearOnEntry = false;
+    updateScreen(calculatorState.number1);
 }
